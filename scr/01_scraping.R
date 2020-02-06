31c12ac1788291acbc3d7ade4fb570e9916b671f
Federica Giua <federica.giua@studenti.unimi.it>
2020-02-06 15:48:07 +0100

    filtering tweets

--- scr/01_scraping.R
+++ scr/01_scraping.R
@@ -2,10 +2,12 @@
 
 #trying to get tweets that contain both coronavirus and fakenews
 tweets_collection <- search_tweets2(
-  c("coronavirus", "fakenews"), 
+  c("coronavirus , fakenews"), 
   n = 50000, 
   retryonratelimit = TRUE, 
-  parse = TRUE
+  parse = TRUE,
+  include_rts = FALSE,
+  lang = "it"
 )
 
 save(tweets_collection, file = "tweets_try1.RData")
