b3874be1d7881496f061b61905954caa0563131a
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-06 10:58:44 +0100

    first try of the code to get data

--- scr/01_scraping.R
+++ scr/01_scraping.R
@@ -1,1 +1,11 @@
 #This is the file dedicated to the collection of data 
+
+#trying to get tweets that contain both coronavirus and fakenews
+tweets_collection <- search_tweets2(
+  c("coronavirus", "fakenews"), 
+  n = 50000, 
+  retryonratelimit = TRUE, 
+  parse = TRUE
+)
+
+save(tweets_collection, file = "data/tweets_try1.RData")
