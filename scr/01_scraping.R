33cf3b5a69e22c47e2379f8d6c3012a28da0c8ac
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-10 12:51:56 +0100

    also, deleted the stream part as it was useless for our purposes, i used it only to understand whether the package was working

--- scr/01_scraping.R
+++ scr/01_scraping.R
@@ -11,16 +11,6 @@ tweets_collection <- search_tweets2(
 
 save(tweets_collection, file = "tweets_try1.RData")
 
-#trying to see what happens with the stream function 
-streamingtweets <- stream_tweets("coronavirus, fakenews", timeout = 30, parse = TRUE, include_rts = FALSE,
-                                 lang = "it")
-
-keywords <- "coronavirus,fakenews"
-streamtime <- 60*5
-stream2 <- stream_tweets(q = keywords, 
-                         timeout = streamtime, 
-                         parse = TRUE) 
-save(stream2, file = "streamedtweets.RData")
 
 #new try with searchtweets
 italian_tweets <- search_tweets2(
