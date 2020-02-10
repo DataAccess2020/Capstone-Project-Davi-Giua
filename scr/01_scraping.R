a92e51008407196f8b305e2b8f07b446965f23a7
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-10 12:51:03 +0100

    made some new attempts with both keywords and then only coronavirus

--- scr/01_scraping.R
+++ scr/01_scraping.R
@@ -24,7 +24,7 @@ save(stream2, file = "streamedtweets.RData")
 
 #new try with searchtweets
 italian_tweets <- search_tweets2(
-  c("coronavirus, fakenews"), n = 1800, 
+  c("coronavirus, fakenews"), n = 50000, 
   retryonratelimit = TRUE,
   include_rts = FALSE, 
   lang = "it"
@@ -32,3 +32,40 @@ italian_tweets <- search_tweets2(
 
 save(italian_tweets, file = "italian_tweets.RData")
 
+english_tweets <- search_tweets2(
+  c("coronavirus, fakenews"), n = 50000, 
+  retryonratelimit = TRUE,
+  include_rts = FALSE, 
+  lang = "en"
+)
+# this retrieves 621 observations (as of 10/02)
+save(english_tweets, file = "english_tweets.RData")
+
+italian_coronav_tweets <- search_tweets2(
+  c("coronavirus"), n = 50000, 
+  retryonratelimit = TRUE,
+  include_rts = FALSE, 
+  lang = "it"
+)
+#this retrieves 51462 observations 
+
+save(italian_coronav_tweets, file = "italian_coronav_tweets.RData")
+
+italian_hashtags_tweets <- search_tweets2(
+  c("#coronavirus, #fakenews"), n = 500, 
+  retryonratelimit = TRUE,
+  include_rts = FALSE, 
+  lang = "it"
+)
+save(italian_hashtags_tweets, file = "italian_hasthag_tweets.RData")
+#this retrieves 235 observations
+
+italian_tweets2 <- search_tweets2(
+  c("coronavirus, fake, news"), n = 18000, 
+  retryonratelimit = TRUE, 
+  include_rts = FALSE, 
+  lang = "it"
+)
+save(italian_tweets2, file = "italian_tweets2.RData")
+
+#this retrieves 579 observations 
