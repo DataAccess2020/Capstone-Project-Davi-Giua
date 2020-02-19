f3b5c0e478322bd9391caabb43744d4233389335
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-19 10:06:04 +0100

    trying to plot

--- scr/03_coronavirus_analysis.R
+++ scr/03_coronavirus_analysis.R
@@ -6,3 +6,12 @@ save(italian_coronavirus_dataset, file = "italian_coronavirus_fulldataset.RData"
 
 #plotting the frequencies of the tweets
 library(ggplot2)
+
+
+frequencies_plot <- ggplot(italian_coronavirus_dataset)) +                    
+  geom_line(aes(y=italian_coronavirus_dataset$status_id), colour="blue")+
+    scale_x_datetime(date_minor_breaks = "1 hour", date_breaks = "1 day")+
+  xlab("02/02/20-18/02/20")+
+  ylab("Number of italian tweets about coronavirus") +
+  labs(title="Italian tweets about coronavirus")+
+  theme_minimal()
