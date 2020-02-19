52b4b44dec8cbfd58ea94a9d357f30b948f614bc
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-19 09:52:07 +0100

    created new file to analyse coronavirus tweets

---
+++ scr/03_coronavirus_analysis.R
@@ -0,0 +1,6 @@
+#working on Coronavirus
+
+#Merging datasets
+italian_coronavirus_dataset <- rbind(italian_coronav_tweets, italian_coronav_tweets1_1802)
+save(italian_coronavirus_dataset, file = "italian_coronavirus_fulldataset.RData")
+
