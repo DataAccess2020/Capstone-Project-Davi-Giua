23a1640a58a8db7d861a0effc9200c5083325c6a
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-19 09:58:25 +0100

    plotting

--- scr/03_coronavirus_analysis.R
+++ scr/03_coronavirus_analysis.R
@@ -4,3 +4,5 @@
 italian_coronavirus_dataset <- rbind(italian_coronav_tweets, italian_coronav_tweets1_1802)
 save(italian_coronavirus_dataset, file = "italian_coronavirus_fulldataset.RData")
 
+#plotting the frequencies of the tweets
+library(ggplot2)
