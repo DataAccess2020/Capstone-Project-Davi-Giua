0da14cf680a5899c57d793a333596207b25a7cfe
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-19 11:04:04 +0100

    fixed and saved frequency plot

--- scr/03_coronavirus_analysis.R
+++ scr/03_coronavirus_analysis.R
@@ -7,7 +7,7 @@ save(italian_coronavirus_dataset, file = "italian_coronavirus_fulldataset.RData"
 #plotting the frequencies of the tweets
 library(ggplot2)
 
-ts_plot(italian_coronavirus_dataset, "1 hour") +
+frequency_plot <- ts_plot(italian_coronavirus_dataset, "1 hour") +
   ggplot2::theme_minimal() +
   scale_x_datetime(date_minor_breaks = "1 day", date_breaks = "1 day", date_labels = "%b %d")+
   ggplot2::theme(plot.title = ggplot2::element_text(face = "bold")) +
@@ -16,5 +16,5 @@ ts_plot(italian_coronavirus_dataset, "1 hour") +
     title = "Frequency of italian tweets relating to coronavirus from Feb 2nd to Feb 18th"
   )
 
-scale_x_date
-scale_x_date(date_labels = "%W")
+ggsave(frequency_plot, filename = "Frequency plot for coronavirus.pdf",
+       device = cairo_pdf, width = 10, height = 4)
