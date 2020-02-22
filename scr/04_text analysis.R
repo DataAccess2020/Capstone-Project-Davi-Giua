5285fc1372d92986bfd93f2f9986b3441a6863e9
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-22 17:10:50 +0100

    added comments

--- scr/04_text analysis.R
+++ scr/04_text analysis.R
@@ -1,4 +1,4 @@
-04_ text coronavirus
+#Let's try and do some text analysis on the tweets relating to coronavirus. 
 
 save(italian_coronavirus_dataset, file = "italian_coroonavirus_dataset_json.json")
 
