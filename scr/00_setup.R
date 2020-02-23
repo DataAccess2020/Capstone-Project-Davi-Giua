03cb0da46ff822c0d17e673540253499d351ab15
Valentina Davì <valentina.davi1@studenti.unimi.it>
2020-02-23 09:38:08 +0100

    added package

--- scr/00_setup.R
+++ scr/00_setup.R
@@ -10,18 +10,3 @@ library(timeSeries)
 
 
 
-
-
-
-
-
-globale <- ggplot(visual0220, aes(visual0220$Giorno)) +                    
-  geom_line(aes(y=visual0220$Sessioni), colour="blue")+
-  geom_line(aes(y=visual0220$Visualizzazioni), colour="darkorange")+ 
-  scale_x_datetime(date_minor_breaks = "1 day", date_breaks = "1 month")+
-  xlab("06/02/19-08/02/20")+
-  ylab("Numero di visualizzazioni") +
-  labs(title="Numero di visualizzazioni YouTube (arancio) e sito (blu)")+
-  theme_minimal()
-
-
